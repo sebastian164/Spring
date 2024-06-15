ece163d261ba3ffbc4495e2443cea8245fadc0d4
sebastian charry <sebastian164164@gmail.com>
2024-06-15 17:09:55 -0500

    feat/project configuration

---
+++ src/main/java/com/charry/market/CharryMarketApplication.java
@@ -0,0 +1,13 @@
+package com.charry.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharryMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharryMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charry/market/CharryMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charry.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharryMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
